a15ae39b457c7e522625f5b5a2fe85a2b2a6368a
vonhat204 <vonhat0000@gmail.com>
2026-07-10 23:10:17 +0700

    add

--- check_sizes.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$dir = "static\images\worklog\week3"
-foreach ($file in Get-ChildItem -Path $dir -Filter *.png) {
-    $img = [System.Drawing.Image]::FromFile($file.FullName)
-    Write-Output "$($file.Name): $($img.Width)x$($img.Height)"
-    $img.Dispose()
-}
